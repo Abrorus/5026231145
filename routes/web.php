7db633119fc3f3b70b74ca9d0e21bd6b1edc446c
Abrorus <abrorusarlandoe@gmail.com>
2025-06-05 16:47:43 +0700

    PERTEMUAN 14

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
 // import java.io; (sama kayak use)
 
 // System.out.println("Hello World");
@@ -67,15 +68,28 @@ Route::get('frontend', function () {
 	return view('frontend');
 });
 
+Route::get('template', function () {
+	return view('template');
+});
+
 Route::get('Dosen', [DosenController::class, 'index']);
 
 Route::get('welcome', [DosenController::class, 'welcome']);
-
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
-Route::get('/formulir', [PegawaiController::class, 'formulir']);
-Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
 
 //route blog
 Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+Route::get('/pegawai/cari','PegawaiController@cari');
+
+// Route::get('pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('formulir', [PegawaiController::class, 'formulir']);
+Route::post('formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/pegawai', [PegawaiDBController::class,'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class,'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class,'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class,'edit']);
+Route::post('/pegawai/update',[PegawaiDBController::class,'update']);
+Route::get('/pegawai/hapus/{id}',[PegawaiDBController::class,'hapus']);
+Route::get('/pegawai/cari',[PegawaiDBController::class,'cari']);
